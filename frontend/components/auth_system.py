390166a4215454406ee520bffadf6c8e589b7c3a
lucasdsantosb86-max <seu-email@gmail.com>
2025-09-08 11:35:27 -0300

    Fix session_state initialization in frontend

--- frontend/components/auth_system.py
+++ frontend/components/auth_system.py
@@ -6,11 +6,13 @@ class AuthSystem:
         self.backend_url = backend_url
         self.init_session_state()
     
-    def init_session_state(self):
-        if 'user' not in st.session_state:
-            st.session_state.user = None
-        if 'access_token' not in st.session_state:
-            st.session_state.access_token = None
+   def init_session_state(self):
+    if 'user' not in st.session_state:
+        st.session_state.user = None
+    if 'access_token' not in st.session_state:
+        st.session_state.access_token = None
+    if 'dashboard_config' not in st.session_state:
+        st.session_state.dashboard_config = None
 
     def is_authenticated(self):
         return st.session_state.user is not None
