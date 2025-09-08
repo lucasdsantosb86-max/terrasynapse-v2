2cc1416f277ef3f965db904bfc7c14302c616294
Terrasynapse IA no Agro <terrasynapse@terrasynapse.com>
2025-09-08 19:51:25 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -29,10 +29,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "https://terrasynapse-frontend.onrender.com",
-        "https://<SEU-DOMÍNIO-CUSTOM>.com"],
+    allow_origins=["http://localhost:3000",
+        "https://terrasynapse-frontend.onrender.com",  # staging
+        "https://terrasynapse-v2.onrender.com",        # prod no Render
+        "https://app.terrasynapse.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
